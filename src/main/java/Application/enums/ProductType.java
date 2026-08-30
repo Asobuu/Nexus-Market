3b93b169b8d6019baa1efec1bf98774273fa7eff
Julian <julian.gomez67@correo.tdea.edu.co>
2026-08-30 12:43:15 -0500

    Add UserStatus and ProductType enums

---
+++ src/main/java/Application/enums/ProductType.java
@@ -0,0 +1,7 @@
+package Application.enums;
+
+// si el producto es fisico necesita envio, si es digital no
+public enum ProductType {
+    PHYSICAL,
+    DIGITAL
+}
---
+++ src/main/java/Application/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package Application.enums;
+
+// estados posibles de una cuenta de usuario
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED
+}
