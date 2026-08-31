5a8ae439d204a03b9cacc36218bfb9c1ef7d56bc
Julian <julian.gomez67@correo.tdea.edu.co>
2026-08-31 00:50:34 -0500

    Add Cart and CartItem entities

---
+++ src/main/java/Application/model/Cart.java
@@ -0,0 +1,53 @@
+package Application.model;
+
+import Application.model.user.Buyer;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// lo que el comprador va guardando antes de decidirse a pagar
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer_id")
+    private Buyer buyer;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "cart_id")
+    private List<CartItem> items = new ArrayList<>();
+
+    public Cart(Buyer buyer) {
+        this.buyer = buyer;
+    }
+
+    public void addItem(CartItem item) {
+        items.add(item);
+    }
+
+    public double getTotal() {
+        double total = 0;
+        for (CartItem item : items) {
+            total += item.getSubtotal();
+        }
+        return total;
+    }
+}
